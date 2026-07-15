f12bee7c47660ac261c0462bf4f41b87dcc2d7a8
Oscar YJH <oscaryjh@gmail.com>
2026-07-16 00:35:03 +0800

    Fix testing build lint error

--- src/app/branches/actions.ts
+++ src/app/branches/actions.ts
@@ -2,7 +2,7 @@
 
 import { requireBusinessUser } from "@/lib/auth/business-user";
 
-export async function updateBranchAction(_formData: FormData) {
+export async function updateBranchAction() {
   await requireBusinessUser();
   throw new Error("Branches are managed by the platform administrator.");
 }
